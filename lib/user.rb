5734c71ac739697340b0f27c39950a5e7d858bd6
Nicholas <mullally.nt@gmail.com>
2022-07-31 14:47:40 -0700

    Done with assignment

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+    def initialize(f_name, l_name)
+        @first_name = f_name
+        @last_name = l_name
+    end
+
+    def getFirstName()
+        @first_name
+    end
+
+    def getLastName()
+        @last_name
+    end
+end
